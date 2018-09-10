3b070fd24586b6ebb4513686b0f89de447fb6de1
Benjamin Leon <benleon3@gmail.com>
2018-09-09 19:42:38 -0700

    amost working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,26 +16,31 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/burgers", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.insertOne([
     "burger_name"
   ], [
     req.body.burger_name
-  ], function(data) {
+  ], function(result) {
     // Send back the ID of the new quote
-    res.redirect('/');
+    res.json({ id: result.insertId });
   });
 });
 
-router.put("/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.updateOne({
-    devoured: true
-  }, condition, function(data) {
-    res.redirect('/');    
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }   
   });
 });
 
